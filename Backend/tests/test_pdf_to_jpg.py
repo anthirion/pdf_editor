2ceb825f2e64ecff00e2ac42f6d78785b7f277e8
thiran <antoth.web@outlook.fr>
2024-12-01 14:27:02 +0100

    [CI/CD] Correction erreur de tests

--- Backend/tests/test_pdf_to_jpg.py
+++ Backend/tests/test_pdf_to_jpg.py
@@ -12,13 +12,15 @@ class TestPDFToJPGConversion(unittest.TestCase):
         self.test_output_folder = Path("Backend/tests/resources/output_jpgs")
         self.test_output_folder.mkdir(exist_ok=False)
         self.test_pdf = Path("Backend/tests/resources/Python-w1.pdf")
-        self.jpg_files = list(self.test_output_folder.glob("*.jpg"))
 
     def tearDown(self) -> None:
         # Nettoyage après les tests
-        for item in self.jpg_files:
+        for item in self.get_jpg_files():
             item.unlink()
         self.test_output_folder.rmdir()
+
+    def get_jpg_files(self):
+        return list(self.test_output_folder.glob("*.jpg"))
 
     def test_pdf_to_jpg_conversion(self) -> None:
         pdf_to_jpg(self.test_pdf, self.test_output_folder)
@@ -26,10 +28,10 @@ class TestPDFToJPGConversion(unittest.TestCase):
         # de pages du pdf d'entrée
         reader = PdfReader(self.test_pdf)
         num_pages = reader.get_num_pages()
-        self.assertEqual(len(self.jpg_files), num_pages)
+        self.assertEqual(len(self.get_jpg_files()), num_pages)
         # Vérification des noms de fichiers générés
         expected_files = {self.test_output_folder / f"page_{i + 1}.jpg" for i in range(num_pages)}
-        actual_files = set(self.jpg_files)
+        actual_files = set(self.get_jpg_files())
         # s'assurer que les fichiers créés correspondent aux fichiers attendus
         self.assertTrue(all([file in expected_files for file in actual_files]))
         self.assertEqual(len(expected_files), len(actual_files))
@@ -39,7 +41,7 @@ class TestPDFToJPGConversion(unittest.TestCase):
         with self.assertRaises(FileNotFoundError):
             pdf_to_jpg(non_existing_pdf, self.test_output_folder)
         # Vérifier qu'il n'y a pas de fichiers JPG créés
-        self.assertEqual(len(self.jpg_files), 0)
+        self.assertEqual(len(self.get_jpg_files()), 0)
 
 
 if __name__ == "__main__":
